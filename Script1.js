54eb8d10b996da0aa501679f84400e818a2aa26b
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-08-28 17:34:17 -0400

    refined shopping menu transitions, added fonts, added scripting effects, and click to scroll to some links to test functionality

--- Script1.js
+++ Script1.js
@@ -1,8 +1,27 @@
+//Click to scroll functionailty
+$(document).ready(function(){
+    $("#section1").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#Tacos").offset().top
+        }, 100);
+    });
+
+    $("#section2").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#Shop").offset().top
+        }, 100);
+    });
+
+});
+
+
+//The fade in effects for the SHOP section
 const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in")
 
 const appearOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -30px 0px"
+    threshold: 0.4,
+    rootMargin: "0px 0px -20px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver 
@@ -23,3 +42,7 @@ const appearOnScroll = new IntersectionObserver
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 })
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
